f8d106482a641a9745e4d1fa2c01508eeef38e9f
sakai-code <76587090+sakai-code@users.noreply.github.com>
2022-03-04 21:49:18 +0900

    aaaaaarebuild

--- src/gui/lib/libraries/extensions/entry/index.jsx
+++ src/gui/lib/libraries/extensions/entry/index.jsx
@@ -1,48 +1,58 @@
-/**
- * This is an extension for Xcratch.
- */
-
-import iconURL from './entry-icon.png';
-import insetIconURL from './inset-icon.svg';
-import translations from './translations.json';
-
-/**
- * Formatter to translate the messages in this extension.
- * This will be replaced which is used in the React component.
- * @param {object} messageData - data for format-message
- * @returns {string} - translated message for the current locale
- */
 let formatMessage = messageData => messageData.defaultMessage;
 
-const entry = {
-    get name () {
-        return formatMessage({
-            id: 'sxratchtest.entry.name',
-            default: 'sxratchtest',
-            description: 'name of the extension'
-        });
+import microbitMoreIconURL from './entry-icon.png';
+import microbitMoreInsetIconURL from './inset-icon.svg';
+import microbitMoreConnectionIconURL from './connection-icon.svg';
+import microbitMoreConnectionSmallIconURL from './connection-small-icon.svg';
+
+const version = 'v2-0.2.4';
+
+const translationMap = {
+    'en': {
+        'gui.extension.microbitMore.description': `Play with all functions of micro:bit. (${version})`
     },
-    extensionId: 'sxratchtest',
-    extensionURL: 'https://sakai-code.github.io/sxratchtest/dist/sxratchtest.mjs',
-    collaborator: 'sakai-code',
-    iconURL: iconURL,
-    insetIconURL: insetIconURL,
+    'ja': {
+        'gui.extension.microbitMore.description': `micro:bitのすべての機能で遊ぶ。 (${version})`
+    },
+    'ja-Hira': {
+        'gui.extension.microbitMore.description': `マイクロビットのすべてのきのうであそぶ。 (${version})`
+    }
+};
+
+const entry = {
+    name: 'Microbit More',
+    extensionId: 'microbitMore',
+    extensionURL: 'https://microbit-more.github.io/dist/microbitMore.mjs',
+    collaborator: 'Yengawa Lab',
+    iconURL: microbitMoreIconURL,
+    insetIconURL: microbitMoreInsetIconURL,
     get description () {
         return formatMessage({
-            defaultMessage: 'an extension for Xcratch',
-            description: 'Description for this extension',
-            id: 'sxratchtest.entry.description'
+            defaultMessage: 'Play with all functions of micro:bit.',
+            description: "Description for the 'Microbit More' extension",
+            id: 'gui.extension.microbitMore.description'
         });
     },
     featured: true,
     disabled: false,
-    bluetoothRequired: false,
+    bluetoothRequired: true,
     internetConnectionRequired: false,
-    helpLink: 'https://sakai-code.github.io/sxratchtest/',
+    launchPeripheralConnectionFlow: true,
+    useAutoScan: false,
+    connectionIconURL: microbitMoreConnectionIconURL,
+    connectionSmallIconURL: microbitMoreConnectionSmallIconURL,
+    get connectingMessage () {
+        return formatMessage({
+            defaultMessage: 'Connecting',
+            description: 'Message to help people connect to their micro:bit.',
+            id: 'gui.extension.microbit.connectingMessage'
+        });
+    },
+    helpLink: 'https://microbit-more.github.io/',
     setFormatMessage: formatter => {
         formatMessage = formatter;
     },
-    translationMap: translations
+    translationMap: translationMap
 };
 
 export {entry}; // loadable-extension needs this line.
